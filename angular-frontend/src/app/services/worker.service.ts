31cb0e9d4d473c253f4663c5500fad528cd1739b
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2025-08-07 18:42:56 +0530

    Update worker.service.ts

--- angular-frontend/src/app/services/worker.service.ts
+++ angular-frontend/src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://13.234.114.236:8080/api/v1/workers";
+  private getUrl: string = "http://43.205.125.38:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
